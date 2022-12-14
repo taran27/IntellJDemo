f5f435297adeb826186de437809ad56cf540c430
TaranJot_Singh <Taran@codebash.us>
2022-12-13 19:52:18 -0800

    Demo_v.1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello again");
 
+        int a = 10;
+        System.out.println(a);
     }
 }
